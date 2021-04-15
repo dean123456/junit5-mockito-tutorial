a1d46ad6a07810d83d5c4627215f71762772b878
Горелкин Илья Евгеньевич <igorelkin@neoflex.ru>
2021-04-15 19:57:17 +0400

    added Parallel tests

---
+++ src/test/java/com/chameleon/code/Parameterized.java
@@ -0,0 +1,20 @@
+package com.chameleon.code;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Parameterized {
+
+    @BeforeEach
+    void init(){
+
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void testWithValueSource(int argument) {
+        assertTrue(argument > 0 && argument < 4);
+    }
+}
